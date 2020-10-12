e3ed78e0e1d55d592d44887880d58bbc55809df6
arsalaerfan <erfanarsala831@gmail.com>
2020-10-12 16:34:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/chatactivity/ChatAdapter.java
@@ -0,0 +1,113 @@
+package com.example.chatactivity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MessageViewHolder> {
+
+    private List<Message> mMessages;
+    private Context mContext;
+    private String mUserId;
+
+    private static final int MESSAGE_OUTGOING = 123;
+    private static final int MESSAGE_INCOMING = 321;
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isMe(position)) {
+            return MESSAGE_OUTGOING;
+        } else {
+            return MESSAGE_INCOMING;
+        }
+    }
+
+    public ChatAdapter(Context context, String userId, List<Message> messages) {
+        mMessages = messages;
+        this.mUserId = userId;
+        mContext = context;
+    }
+
+    private boolean isMe(int position) {
+        Message message = mMessages.get(position);
+        return message.getUserId() != null && message.getUserId().equals(mUserId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessages.size();
+    }
+
+
+    @Override
+    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        if (viewType == MESSAGE_INCOMING) {
+            View contactView = inflater.inflate(R.layout.message_incoming, parent, false);
+            return new IncomingMessageViewHolder(contactView);
+        } else if (viewType == MESSAGE_OUTGOING) {
+            View contactView = inflater.inflate(R.layout.messages_outgoing, parent, false);
+            return new OutgoingMessageViewHolder(contactView);
+        } else {
+            throw new IllegalArgumentException("Unknown view type");
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(MessageViewHolder holder, int position) {
+        // TODO: implement method
+    }
+
+    public abstract class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        abstract void bindMessage(Message message);
+    }
+
+    public class IncomingMessageViewHolder extends MessageViewHolder {
+        ImageView imageOther;
+        TextView body;
+        TextView name;
+
+        public IncomingMessageViewHolder(View itemView) {
+            super(itemView);
+            imageOther = (ImageView) itemView.findViewById(R.id.ivProfileOther);
+            body = (TextView) itemView.findViewById(R.id.tvBody);
+            name = (TextView) itemView.findViewById(R.id.tvName);
+        }
+
+        @Override
+        public void bindMessage(Message message) {
+            // TODO: implement later
+        }
+    }
+
+    public class OutgoingMessageViewHolder extends MessageViewHolder {
+        ImageView imageMe;
+        TextView body;
+
+        public OutgoingMessageViewHolder(View itemView) {
+            super(itemView);
+            imageMe = (ImageView) itemView.findViewById(R.id.ivProfileMe);
+            body = (TextView) itemView.findViewById(R.id.tvBody);
+        }
+
+        @Override
+        public void bindMessage(Message message) {
+            // TODO: implement later
+        }
+    }
+}
